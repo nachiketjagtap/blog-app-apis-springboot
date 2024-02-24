3c2b56e0dab90e281caca6830bb6848286334320
nachiket <nachiketjagtap9881@gmail.com>
2024-02-24 22:43:39 +0530

    Done Create Use API

---
+++ src/main/java/com/nachiket/blog/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.nachiket.blog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nachiket.blog.payloads.UserDto;
+import com.nachiket.blog.services.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	//POST - create user
+	@PostMapping("/")
+	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+		UserDto createUserDto = this.userService.craeteUser(userDto);
+		return  new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
+		
+	}
+	//PUT -update user
+	
+	//DELETE = delete user
+	
+	
+	//GET - get user
+
+}
--- src/main/java/com/nachiket/blog/services/impl/UserServiceImpl.java
+++ src/main/java/com/nachiket/blog/services/impl/UserServiceImpl.java
@@ -1,14 +1,18 @@
 package com.nachiket.blog.services.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.nachiket.blog.exceptions.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.nachiket.blog.entities.User;
 import com.nachiket.blog.payloads.UserDto;
 import com.nachiket.blog.repositories.UserRepo;
 import com.nachiket.blog.services.UserService;
-
+@Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepo userRepo;
@@ -24,25 +28,46 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public UserDto updateUser(UserDto user, Integer id) {
+	public UserDto updateUser(UserDto userDto, Integer id) {
 		// TODO Auto-generated method stub
-		return null;
+		User user = this.userRepo.findById(id).orElseThrow(()->new ResourceNotFoundException("User"," id ",id));
+		
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setPassword(userDto.getPassword());
+		user.setAbout(userDto.getAbout());
+		
+		User updatedUser=this.userRepo.save(user);
+		
+		UserDto userDto1=this.userToDto(updatedUser);
+		
+		return userDto1;
 	}
 
 	@Override
 	public UserDto getUserById(Integer id) {
 		// TODO Auto-generated method stub
-		return null;
+		User user = this.userRepo.findById(id).orElseThrow(()->new ResourceNotFoundException("User"," id ",id));
+		
+		
+		return this.userToDto(user);
 	}
 
 	@Override
 	public List<UserDto> getllUsers() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		List<User> users = this.userRepo.findAll();
+		List<UserDto> userDtos = users.stream().map(user->this.userToDto(user)).collect(Collectors.toList());
+		
+		return userDtos;
 	}
 
 	@Override
 	public void deleteUser(Integer UserId) {
+		
+		User user = this.userRepo.findById(UserId).orElseThrow(()->new ResourceNotFoundException("User"," id ",UserId));
+		this.userRepo.delete(user);
 		// TODO Auto-generated method stub
 
 	}
