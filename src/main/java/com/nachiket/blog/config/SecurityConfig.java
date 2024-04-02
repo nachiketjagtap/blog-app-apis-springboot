1c731fa4fce399e420c32ff51cc9b8fa46bb4657
nachiket <nachiketjagtap9881@gmail.com>
2024-04-02 09:30:19 +0530

    Implemnted JWT authentication...!

--- src/main/java/com/nachiket/blog/config/SecurityConfig.java
+++ src/main/java/com/nachiket/blog/config/SecurityConfig.java
@@ -3,14 +3,20 @@ package com.nachiket.blog.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.nachiket.blog.security.CustomeUserDetailService;
+import com.nachiket.blog.security.JwtAuthenticationEntryPoint;
+import com.nachiket.blog.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +24,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private CustomeUserDetailService customUserDetailService ; 
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -25,10 +37,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf()
 			.disable()
 			.authorizeHttpRequests()
+			.antMatchers("/api/v1/auth/login").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
-			.httpBasic();
+			.exceptionHandling().authenticationEntryPoint(this. jwtAuthenticationEntryPoint)
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Override
+	@Bean 
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
 	}
 
 	@Override
